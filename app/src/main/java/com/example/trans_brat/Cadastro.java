5560e6043504c942204eb0880fb05b9258ec8fe8
Maria <mariaeduardaparreirads@gmail.com>
2024-08-16 09:13:34 -0300

    tela de entra, tela de cadastro e fazendo tela de navegação

---
+++ app/src/main/java/com/example/trans_brat/Cadastro.java
@@ -0,0 +1,46 @@
+package com.example.trans_brat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Cadastro extends AppCompatActivity {
+
+    private TextView Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cadastro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        IniciarComponentes();
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Cadastro.this,login.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+        Login = findViewById(R.id.login);
+    }
+
+}
