96a266e0ca9e4a1c9e82eeae594eae98ea055f7f
Eric Nunes Ramalho <eric26052004@gmail.com>
2024-08-20 22:12:56 -0300

    Validação de login e cadastro

--- app/src/main/java/com/example/trans_brat/CadastroActivity.java
+++ app/src/main/java/com/example/trans_brat/CadastroActivity.java
@@ -1,13 +1,20 @@
 package com.example.trans_brat;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.transition.Fade;
 import android.transition.Slide;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +23,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CadastroActivity extends AppCompatActivity {
+    private boolean isPasswordVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +39,136 @@ public class CadastroActivity extends AppCompatActivity {
             return insets;
         });
 
+        EditText inputNome = findViewById(R.id.input_name); // Input de nome
+        EditText inputMatricula = findViewById(R.id.input_matricula); // Input de matrícula
+        EditText inputSenha = findViewById(R.id.input_password); // Input de senha
+        Button buttonCadastro = findViewById(R.id.button_cadastro); // Botão de confirmar cadastro
+
+        /* Ao clicar no ícone de exibir senha */
+        inputSenha.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_RIGHT = 2;
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (inputSenha.getRight() - inputSenha.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        togglePasswordVisibility(inputSenha);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        /* Ao clicar no botão de confirmar cadastro */
+        buttonCadastro.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                /* Obtendo valores dos inputs de matrícula e senha */
+                validateCadastro(inputNome, inputMatricula, inputSenha);
+            }
+        });
+
+        /* Alterando para a tela de login */
         Button buttonCadastroRedirect = findViewById(R.id.cadastro_redirect);
         buttonCadastroRedirect.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(CadastroActivity.this, LoginActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
+    }
+
+    /* Função para validar cadastro */
+    private void validateCadastro(EditText inputNome, EditText inputMatricula, EditText inputSenha) {
+        /* Obtendo textos dos inputs */
+        String nome = inputNome.getText().toString().trim();
+        String matricula = inputMatricula.getText().toString().trim();
+        String senha = inputSenha.getText().toString().trim();
+
+        /* Se o nome não for informado */
+        if (nome.isEmpty()) {
+            Toast.makeText(CadastroActivity.this, "Insira o seu nome", Toast.LENGTH_SHORT).show();
+            setInputErrorBorder(inputNome);
+            return;
+        }
+        removeInputErrorBorders(new EditText[] {inputNome, inputMatricula, inputSenha});
+
+        /* Se a matrícula não for informada */
+        if (matricula.isEmpty()) {
+            Toast.makeText(CadastroActivity.this, "Insira a sua matrícula", Toast.LENGTH_SHORT).show();
+            setInputErrorBorder(inputMatricula);
+            return;
+        }
+
+        /* Verifica se a matrícula tem o mínimo de caracteres necessários e se são apenas números */
+        int matriculaRequired = 12;
+        if (matricula.length() < matriculaRequired) {
+            Toast.makeText(CadastroActivity.this, "Sua matrícula precisa ter " + matriculaRequired + " caracteres", Toast.LENGTH_SHORT).show();
+            setInputErrorBorder(inputMatricula);
+            return;
+        }
+        removeInputErrorBorders(new EditText[] {inputNome, inputMatricula, inputSenha});
+
+        /* Se a senha não for informada */
+        if (senha.length() == 0) {
+            Toast.makeText(CadastroActivity.this, "Insira a sua senha", Toast.LENGTH_SHORT).show();
+            setInputErrorBorder(inputSenha);
+            return;
+        }
+
+        /* Verifica se a senha tem no mínimo 8 caracteres */
+        int senhaRequired = 8;
+        if (senha.length() < 8) {
+            Toast.makeText(CadastroActivity.this, "Sua senha precisa ter " + senhaRequired + " caracteres", Toast.LENGTH_SHORT).show();
+            setInputErrorBorder(inputSenha);
+            return;
+        }
+        removeInputErrorBorders(new EditText[] {inputNome, inputMatricula, inputSenha});
+
+        Intent intent = new Intent(CadastroActivity.this, HomeActivity.class);
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(CadastroActivity.this).toBundle());
+        finish();
+    }
+
+    /* Função para adicionar borda vermelha em um input */
+    public void setInputErrorBorder(EditText editText) {
+        editText.setBackgroundResource(R.drawable.edit_text_login_error);
+        Drawable drawableRight = editText.getCompoundDrawables()[2];
+        if (drawableRight != null) {
+            drawableRight.setTint(getResources().getColor(R.color.red));
+        }
+    }
+
+    /* Função para remover borda vermelha em um input */
+    public void removeInputErrorBorder(EditText editText) {
+        editText.setBackgroundResource(R.drawable.edit_text_login);
+        Drawable drawableRight = editText.getCompoundDrawables()[2];
+        if (drawableRight != null) {
+            drawableRight.setTint(getResources().getColor(R.color.black));
+        }
+    }
+
+    /* Função para remover bordas vermelhas de todos os input */
+    public void removeInputErrorBorders(EditText[] editTexts) {
+        for(EditText editText : editTexts) {
+            removeInputErrorBorder(editText);
+        }
+    }
+
+    /* Função para visualizar senha quando o usuário clicar no ícone à direita */
+    private void togglePasswordVisibility(EditText editText) {
+        if (isPasswordVisible) {
+            /* Ocultando senha */
+            editText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            editText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.icon_eye_closed, 0);
+        } else {
+            /* Mostrando senha */
+            editText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            editText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.icon_eye_open, 0);
+        }
+        isPasswordVisible = !isPasswordVisible;
+
+        /* Colocando o cursor no final do texto */
+        editText.setSelection(editText.getText().length());
     }
 }
--- app/src/main/java/com/example/trans_brat/LoginActivity.java
+++ app/src/main/java/com/example/trans_brat/LoginActivity.java
@@ -2,13 +2,21 @@ package com.example.trans_brat;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.transition.Fade;
 import android.transition.Slide;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +25,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    private boolean isPasswordVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,20 +41,127 @@ public class LoginActivity extends AppCompatActivity {
             return insets;
         });
 
-        Button buttonNavegacaoRedirect = findViewById(R.id.button_login);
-        buttonNavegacaoRedirect.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this).toBundle());
+        EditText inputMatricula = findViewById(R.id.input_matricula); // Input de matrícula
+        EditText inputSenha = findViewById(R.id.input_password); // Input de senha
+        Button buttonLogin = findViewById(R.id.button_login); // Botão de confirmar login
+
+        /* Ao clicar no ícone de exibir senha */
+        inputSenha.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_RIGHT = 2;
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (inputSenha.getRight() - inputSenha.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        togglePasswordVisibility(inputSenha);
+                        return true;
+                    }
+                }
+                return false;
             }
         });
 
+        /* Ao clicar no botão de confirmar login */
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                /* Obtendo valores dos inputs de matrícula e senha */
+                validateLogin(inputMatricula, inputSenha);
+
+            }
+        });
+
+        /* Alterando para a tela de cadastro */
         Button buttonLoginRedirect = findViewById(R.id.login_redirect);
         buttonLoginRedirect.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
                 startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this).toBundle());
+                finish();
             }
         });
+    }
+
+    /* Função para validar login */
+    private void validateLogin(EditText inputMatricula, EditText inputSenha) {
+        /* Obtendo textos dos inputs */
+        String matricula = inputMatricula.getText().toString().trim();
+        String senha = inputSenha.getText().toString().trim();
+
+        /* Se a matrícula não for informada */
+        if (matricula.isEmpty()) {
+            Toast.makeText(LoginActivity.this, "Insira a sua matrícula", Toast.LENGTH_SHORT).show();
+            setInputErrorBorder(inputMatricula);
+            return;
+        }
+
+        /* Verifica se a matrícula tem o mínimo de caracteres necessários e se são apenas números */
+        int matriculaRequired = 12;
+        if (matricula.length() < matriculaRequired) {
+            Toast.makeText(LoginActivity.this, "Sua matrícula precisa ter " + matriculaRequired + " caracteres", Toast.LENGTH_SHORT).show();
+            setInputErrorBorder(inputMatricula);
+            return;
+        }
+        removeInputErrorBorders(new EditText[] {inputMatricula, inputSenha});
+
+        /* Se a senha não for informada */
+        if (senha.length() == 0) {
+            Toast.makeText(LoginActivity.this, "Insira a sua senha", Toast.LENGTH_SHORT).show();
+            setInputErrorBorder(inputSenha);
+            return;
+        }
+
+        /* Verifica se a senha tem no mínimo 8 caracteres */
+        int senhaRequired = 8;
+        if (senha.length() < 8) {
+            Toast.makeText(LoginActivity.this, "Sua senha precisa ter " + senhaRequired + " caracteres", Toast.LENGTH_SHORT).show();
+            setInputErrorBorder(inputSenha);
+            return;
+        }
+        removeInputErrorBorders(new EditText[] {inputMatricula, inputSenha});
+
+        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this).toBundle());
+        finish();
+    }
+
+    /* Função para adicionar borda vermelha em um input */
+    public void setInputErrorBorder(EditText editText) {
+        editText.setBackgroundResource(R.drawable.edit_text_login_error);
+        Drawable drawableRight = editText.getCompoundDrawables()[2];
+        if (drawableRight != null) {
+            drawableRight.setTint(getResources().getColor(R.color.red));
+        }
+    }
+
+    /* Função para remover borda vermelha em um input */
+    public void removeInputErrorBorder(EditText editText) {
+        editText.setBackgroundResource(R.drawable.edit_text_login);
+        Drawable drawableRight = editText.getCompoundDrawables()[2];
+        if (drawableRight != null) {
+            drawableRight.setTint(getResources().getColor(R.color.black));
+        }
+    }
+
+    /* Função para remover bordas vermelhas de todos os input */
+    public void removeInputErrorBorders(EditText[] editTexts) {
+        for(EditText editText : editTexts) {
+            removeInputErrorBorder(editText);
+        }
+    }
+
+    /* Função para visualizar senha quando o usuário clicar no ícone à direita */
+    private void togglePasswordVisibility(EditText editText) {
+        if (isPasswordVisible) {
+            /* Ocultando senha */
+            editText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            editText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.icon_eye_closed, 0);
+        } else {
+            /* Mostrando senha */
+            editText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            editText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.icon_eye_open, 0);
+        }
+        isPasswordVisible = !isPasswordVisible;
+
+        /* Colocando o cursor no final do texto */
+        editText.setSelection(editText.getText().length());
     }
 }
